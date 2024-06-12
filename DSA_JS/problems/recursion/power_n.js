217bbfc64397b9a9d37a47abc6f80191c84f4e3a
suryakanta007 <sahusuryakanta073@gmail.com>
2024-06-12 10:19:48 +0530

    Fix folder Structure.

--- DSA_JS/problems/recursion/power_n.js
+++
@@ -1,7 +0,0 @@
-const power = (x,n)=>{
-    if(n==1||n==0){
-        return x;
-    }
-    return power(x,n-1)*x;
-}
-console.log(power(2,3));
--- DSA_JS/problems/recursion/sum_of_n.js
+++
@@ -1,8 +0,0 @@
-const sum = (n)=>{
-    if(n===1){
-        return 1
-    }
-    return sum(n-1)+n;
-}
-
-console.log(sum(3));
