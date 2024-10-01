85a0ae0e27b30889139c7555c1385a04b1f4f974
suryakanta007 <sahusuryakanta073@gmail.com>
2024-10-01 22:21:39 +0530

    today i soved a easy mearge the string alternatively problem.

--- Revision_from_start/mearge_String_alternatively.js
+++ Revision_from_start/mearge_String_alternatively.js
@@ -1,3 +1,22 @@
 var mergeAlternately = function(word1, word2) {
-    
+    let mearged = "";
+    let i = 0;
+    let j  = 0;
+    while(i<word1.length&&j<word2.length){
+        mearged +=  word1[i];
+        mearged +=  word2[j];
+        i++
+        j++
+    }
+    // add remaing charters for word1
+    while(i<word1.length){
+        mearged += word1[i]
+        i++
+    }
+    while(j<word2.length){
+        mearged +=  word2[j]
+        j++
+    }
+    return mearged;
 };
+console.log( mergeAlternately("abc","pqruv"));
